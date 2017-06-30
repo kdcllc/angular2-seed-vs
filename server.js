478503b418621003bf9cad23026108838eb19771
KDCLLC <info@kingdavidconsulting.com>
2017-06-30 13:38:35 -0400

    add support for debugging

---
+++ server.js
@@ -0,0 +1,27 @@
+﻿//ADDED
+var exec = require('child_process').exec;
+
+exec('webpack-dev-server --open --inline --colors --progress --display-error-details --display-cached --port 8080 --content-base src', function(err, stdout, stderr) {
+if (err) {
+    console.error(err);
+    return;
+}
+    console.log(stdout);
+});
+
+//var WebpackDevServer = require("webpack-dev-server");
+//var config = require('./webpack.config.js');
+
+//new WebpackDevServer(webpack(config), {
+//    contentBase: 'src',
+//    hot: true,
+//    debug: true
+//}).listen(port, host, function (err, result) {
+//    if (err) {
+//        console.log(err);
+//    }
+//});
+//console.log('-------------------------');
+//console.log('Local web server runs at http://' + host + ':' + port);
+//console.log('-------------------------');
+
